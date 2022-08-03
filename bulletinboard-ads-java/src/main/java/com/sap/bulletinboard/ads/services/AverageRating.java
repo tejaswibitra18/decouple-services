562f38dd946983f2327d819a5276d33a60785e12
D068017 <marcel.dambach@sap.com>
2022-08-03 07:52:08 +0200

    Remove unnecessary json annotations

--- bulletinboard-ads-java/src/main/java/com/sap/bulletinboard/ads/services/AverageRating.java
+++ bulletinboard-ads-java/src/main/java/com/sap/bulletinboard/ads/services/AverageRating.java
@@ -1,16 +1,12 @@
 package com.sap.bulletinboard.ads.services;
 
-import com.fasterxml.jackson.annotation.JsonProperty;
-
 public class AverageRating {
     private Double averageRating;
     
-    @JsonProperty("averageRating")
     public Double getAverageRating() {
         return averageRating;
     }
 
-    @JsonProperty("averageRating")
     public void setAverageRating(Double averageRating) {
         this.averageRating = averageRating;
     }
